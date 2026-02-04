28215d604a99190c631066dd896b9f186619cd7e
RonakVaghela1902 <ronakvaghela1902@gmail.com>
2026-02-04 21:44:47 +0530

    Add AuthorName Column in Book Table

--- BookShoppingCart/Models/Book.cs
+++ BookShoppingCart/Models/Book.cs
@@ -11,6 +11,9 @@ namespace BookShoppingCart.Models
         [MaxLength(40)]
         public string? BookName { get; set; }
         [Required]
+        [MaxLength(40)]
+        public string? AuthorName { get; set; }
+        [Required]
         public double Price { get; set; }
         public string? Image { get; set; }
         [Required]
