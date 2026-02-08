9d98d8ae80506d743c58b82357e8025776c49668
RonakVaghela1902 <ronakvaghela1902@gmail.com>
2026-02-08 17:05:03 +0530

    "Add to Cart" JS feature

---
+++ BookShoppingCart/wwwroot/js/cart.js
@@ -0,0 +1,14 @@
+﻿async function add(bookId) {
+    try {
+        let response = await fetch(`/Cart/AddItem?bookId=${bookId}`);
+        if (response.status == 200) {
+            let result = await response.json();
+            console.log(result);
+            let cartCountEl = document.getElementById("cartCount");
+            cartCountEl.innerHTML = result;
+        }
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
